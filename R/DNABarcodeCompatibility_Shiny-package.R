d6bfa79dfe114cbdb900e9f379f9dd37f5fc6f87
etournay <raphael.etournay@pasteur.fr>
2019-06-20 15:21:06 +0200

    initial package set up

---
+++ R/DNABarcodeCompatibility_Shiny-package.R
@@ -0,0 +1,26 @@
+#' DNABarcodeCompatibility_Shiny: 
+#' A web app to find optimised sets of compatible barcodes with least 
+#' heterogeneity in barcode usage for multiplex
+#' experiments performed on next generation sequencing platforms.
+#'
+#' 
+#' The DNABarcodeCompatibility package provides six functions to load DNA 
+#' barcodes, and to generate, filter and optimise sets of barcode combinations 
+#' for multiplex sequencing experiments.
+#' In particular, barcode combinations are selected to be compatible with 
+#' respect to Illumina chemistry constraints, and can be filtered to keep those
+#' that are robust against substitution and insertion/deletion errors 
+#' thereby facilitating the demultiplexing step.
+#' In addition, the package provides an optimiser function to further favor
+#' the selection of compatible barcode combinations with least 
+#' heterogeneity in barcode usage.
+#'    
+#' @docType package
+#' @name DNABarcodeCompatibility_Shiny-package
+#' @import shiny
+#' @import shinydashboard
+#' @import DT
+#' @import shinyjs
+#' @import stringr
+#' @import DNABarcodeCompatibility
+NULL
