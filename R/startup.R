23e92d56444b07d98b9ae548ea93f666665a288f
etournay <raphael.etournay@pasteur.fr>
2019-06-20 16:12:31 +0200

    fixed startup and added documentation

---
+++ R/startup.R
@@ -0,0 +1,21 @@
+#' @title 
+#' Start up the DNABarcodeCompatibilityShiny interface
+#'
+#' @description 
+#' Start up the DNABarcodeCompatibilityShiny interface
+#'
+#' @usage 
+#' startup()
+#'
+#' @return 
+#' Launches the web interface
+#'
+#' @examples
+#' startup()
+#'
+#' @export
+#' 
+
+startup <- function(){
+    shiny::runApp('R')
+}
